ba817d18021d427a7d726ed8546eafdf95f03d80
Kunal Indoliya <kunalindoliya@gmail.com>
2018-09-28 01:59:26 +0530

    Implement Interfaces for Services. closes #5

---
+++ pet-clinic-data/src/main/java/kunalspringframework/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package kunalspringframework.petclinic.services;
+
+import kunalspringframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet owner);
+    Set<Vet> findAll();
+}
