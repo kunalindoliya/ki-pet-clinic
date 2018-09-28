45a3ee64c3bd2b3ebd8d5bec5708ac6754f2f038
Kunal Indoliya <kunalindoliya@gmail.com>
2018-09-28 23:16:35 +0530

    Refactoring service interfaces.

--- pet-clinic-data/src/main/java/kunalspringframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/kunalspringframework/petclinic/services/VetService.java
@@ -2,10 +2,7 @@ package kunalspringframework.petclinic.services;
 
 import kunalspringframework.petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long> {
+
 }
