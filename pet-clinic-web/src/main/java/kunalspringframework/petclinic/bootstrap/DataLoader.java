5114373ef02c9c4a46562eb404cf776fc0019efd
Kunal Indoliya <kunalindoliya@gmail.com>
2018-10-02 23:44:11 +0530

    Copy all static resources. closes #16

--- pet-clinic-web/src/main/java/kunalspringframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kunalspringframework/petclinic/bootstrap/DataLoader.java
@@ -22,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1=new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michale");
         owner1.setLastName("waston");
         ownerService.save(owner1);
 
         Owner owner2=new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Danny");
         owner2.setLastName("Rand");
         ownerService.save(owner2);
@@ -36,14 +34,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet1=new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2=new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Ward");
         vet2.setLastName("Meechum");
 
