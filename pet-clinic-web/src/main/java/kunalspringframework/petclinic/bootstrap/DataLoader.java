375d8e38a41cc1729f82353c4bfaaeba1f86a48e
Kunal Indoliya <kunalindoliya@gmail.com>
2018-10-04 00:57:55 +0530

    Added PetType data on startup. closes #30

--- pet-clinic-web/src/main/java/kunalspringframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kunalspringframework/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package kunalspringframework.petclinic.bootstrap;
 
 import kunalspringframework.petclinic.model.Owner;
+import kunalspringframework.petclinic.model.PetType;
 import kunalspringframework.petclinic.model.Vet;
 import kunalspringframework.petclinic.services.OwnerService;
+import kunalspringframework.petclinic.services.PetTypeService;
 import kunalspringframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService){
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService){
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog=new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType=petTypeService.save(dog);
+
+        PetType cat=new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType=petTypeService.save(cat);
 
         Owner owner1=new Owner();
         owner1.setFirstName("Michale");
