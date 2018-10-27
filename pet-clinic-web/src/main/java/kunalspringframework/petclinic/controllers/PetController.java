4e5a77e92ddfd88e3d36edf70b5135fc9dce3e85
Kunal Indoliya <kunalindoliya@gmail.com>
2018-10-27 13:53:30 +0530

    Adding model attribute for pet and pet type. #55.

---
+++ pet-clinic-web/src/main/java/kunalspringframework/petclinic/controllers/PetController.java
@@ -0,0 +1,47 @@
+package kunalspringframework.petclinic.controllers;
+
+/* Created by Kunal Indoliya */
+
+import kunalspringframework.petclinic.model.Owner;
+import kunalspringframework.petclinic.model.PetType;
+import kunalspringframework.petclinic.services.OwnerService;
+import kunalspringframework.petclinic.services.PetService;
+import kunalspringframework.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
